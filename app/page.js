9afb47726e1e4d1c8ef3fcc1ab5772d5b616f703
ChaseRensberger <chasehrensberger@gmail.com>
2022-11-17 02:57:21 -0500

    simulation progress

--- app/page.js
+++ app/page.js
@@ -6,35 +6,30 @@ import { Chess } from 'chess.js';
 const Main = () => {
 	const [chess, setChess] = useState(new Chess());
 	const [updatedPosition, setUpdatedPosition] = useState();
-	const [halfMoves, setHalfMoves] = useState(0);
-	const maxHalfMoves = 500;
+	const [simulationState, setSimulationState] = useState(false);
 
 	const makeRandomMove = () => {
 		const moves = chess.moves();
 		const move = moves[Math.floor(Math.random() * moves.length)];
 		chess.move(move);
 		setUpdatedPosition(chess.fen());
-		setHalfMoves(halfMoves + 1);
-		console.log(halfMoves);
 	};
 
-	const simulate = () => {
-		if (halfMoves < maxHalfMoves) {
-			makeRandomMove();
+	useEffect(() => {
+		if (!chess.isGameOver() && simulationState) {
+			setTimeout(makeRandomMove, 200);
 		}
-
-		setTimeout(simulate, 10);
-	};
+	}, [updatedPosition, simulationState]);
 
 	return (
 		<div className="page">
 			<Chessboard position={updatedPosition} />
 			<button
 				onClick={() => {
-					simulate();
+					setSimulationState(!simulationState);
 				}}
 			>
-				Start Simulation
+				Toggle Simulation
 			</button>
 		</div>
 	);
