5d471da972cf08e45a9a3650fe339f36a68b9ff9
ChaseRensberger <chasehrensberger@gmail.com>
2022-11-23 18:38:56 -0500

    begin UI

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
+'use client';
 import '../styles/globals.css';
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function RootLayout({ children }) {
 	return (
 		<html>
 			<head></head>
-			<body>{children}</body>
+			<body>
+				<NextUIProvider>{children}</NextUIProvider>
+			</body>
 		</html>
 	);
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
+'use client';
+import { Button, Text } from '@nextui-org/react';
+
 const Home = () => {
 	return (
-		<div>
-			<div>Hey</div>
+		<div className="page">
+			<Text
+				h1
+				css={{
+					textGradient: '45deg, $blue600 -20%, $pink600 50%',
+				}}
+				weight="bold"
+			>
+				Welcome to Next-Chess!
+			</Text>
+			<Text h2 color="white">
+				You can use this to play/simulate engines right in your browser
+			</Text>
+			<Text h2 color="white">
+				This project is currently under development
+			</Text>
 		</div>
 	);
 };
